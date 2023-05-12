e89f15e7ecd4bde073b6a40b27472de58fd755af
bossa96demo <ilyaershov09@gmail.com>
2023-05-12 20:38:30 +0300

    alacritty was made to be absolutely best

--- polybar/scripts/music.sh
+++ polybar/scripts/music.sh
@@ -2,9 +2,8 @@ abob=$(mpris-ctl info | cut -d "-" -f 1)
 aboba=$(mpris-ctl info | rev | cut -d "-" -f 1 | rev)
 aaa=$(echo $abob - $aboba)
 is=$(echo $aaa | wc -c)
-if [ $is -gt 30 ];then
+if [ $is -gt 42 ];then
 	echo $aaa | head -c 42 | sed 's/.\{1\}$/…/'
 else
 	echo $aaa
 fi
-exit 0
