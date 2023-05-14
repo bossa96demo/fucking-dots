8cf65fc370c719dbb22fca0abdac4fd8c397c520
bossa96demo <ilyaershov09@gmail.com>
2023-05-14 21:50:28 +0300

    added script that will add everything into dots, alacritty changed a bit and rofi updated

---
+++ copy.sh
@@ -0,0 +1,6 @@
+cp -r ~/.config/fish/ ~/gth-s/fucking-dots/
+cp -r ~/.config/alacritty/ ~/gth-s/fucking-dots/
+cp -r ~/.config/bspwm/ ~/gth-s/fucking-dots/
+cp -r ~/.config/sxhkd/ ~/gth-s/fucking-dots/
+cp -r ~/.config/polybar/ ~/gth-s/fucking-dots/
+cp -r ~/.config/rofi/ ~/gth-s/fucking-dots/
