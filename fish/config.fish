bcdc14f253487806f7105a4f0af2685d1fe65e3c
bossa96demo <ilyaershov09@gmail.com>
2023-05-21 23:34:57 +0300

    more aliases

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 function fish_prompt
-	echo -n -s (set_color cyan)"["(prompt_pwd)"]" (set_color normal)" \$ "
+	echo -n -s (set_color green)"["(prompt_pwd)"]" (set_color normal)" \$ "
 end
 function fish_right_prompt
 	echo "took $CMD_DURATION ms"
@@ -28,9 +28,6 @@ end
 function cmt
 	git commit -m "$argv"
 end
-function ada
-	git add -A
-end
 function add
 	git add $argv
 end
@@ -38,21 +35,27 @@ function psh
 	git push
 end
 function dog # DO Git
-	ada; cmt $argv; psh
+	git add -A; cmt $argv; psh
 end
 
+# other useful things
 function fih # Find In History - fih
 	history $argv
 end
 
 function upd # update system
-	sudo pacman -Syu --noconfirm; shutdown
+	sudo pacman -Syu; shutdown
 end
 
+function cls # clear and ls
+	clear;ls
+end
+function car # Compile And Run -  car
+	gcc $argv && ./a.out
+end
+
+# fun
 function 西瓜
 	yes "西瓜/watermelon/арбуз/󱁇"
 end
 
-function cls
-	clear;ls
-end
