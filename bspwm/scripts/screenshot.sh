1fb20ac52937882fcd4a88f9097d386f2c602401
bossa96demo <ilyaershov09@gmail.com>
2023-05-21 23:39:21 +0300

    minor changes, better screenshot

--- bspwm/scripts/screenshot.sh
+++ bspwm/scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 photo="screenshot-$(date +%F_%T).png"
 file="$(xdg-user-dir)/screenshot-s/$photo"
-maim -s | xclip -selection clipboard -target image/png -i
+maim -u -s | xclip -selection clipboard -target image/png -i
