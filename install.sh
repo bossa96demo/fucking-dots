9c87a52cfde10b9daa1c82d5df72e4fa363c7250
bossa96demo <ilyaershov09@gmail.com>
2023-06-01 23:41:07 +0300

    script to copy all config-s to your cfg folder

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp -r ~/gth-s/fucking-dots/fish			~/.config/
+cp -r ~/gth-s/fucking-dots/alacritty/ 		~/.config/
+cp -r ~/gth-s/fucking-dots/bspwm/ 		~/.config/
+cp -r ~/gth-s/fucking-dots/sxhkd/ 		~/.config/
+cp -r ~/gth-s/fucking-dots/polybar/		~/.config/
+cp -r ~/gth-s/fucking-dots/rofi			~/.config
+cp ~/gth-s/fucking-dots/.xinitrc		~/
+cp ~/gth-s/fucking-dots/.vimrc			~/
